d0824b8995fd49c97844d6b40965d41b6fd794e9
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-02-24 20:35:16 -0800

    created auth.js and passport.js, created dependencies

---
+++ src/auth.js
@@ -0,0 +1,33 @@
+var jwtSecret = 'your_jwt_secret'; // Same key used in jwtStrategy
+var jwt = require('jsonwebtoken');
+const passport = require('passport');
+// require('./passport.js') // Local passport file
+
+function generateJWTToken(user) {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, // Username you are encoding
+        expiresIn: '7d', // Durationg of token
+        algorithm: 'HS256' // Encoding the values of the JWT
+    });
+}
+
+/* POST login */
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session : false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                var token = generateJWTToken(user.toJSON());
+                return res.json({ user, token }); // ES6 shorthand if the keys are the same as the values
+            });
+        })(req, res);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+require('./passport.js');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const auth = require('./auth.js')(app); // Make sure this is after bodyParser, app ensures Express is available in auth.js as well
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+app.get('/login', (req, res) => {
+    let login = path.join(__dirname, '../public/login.html');
+    fs.readFile(login, (err, data) => {
+        if (err) {
+            res.status(500);
+            res.send(`Error: ${err}`);
+            return;
+        }
+        res.send(data.toString());
+    });
+});
+
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send('Error: ' + error);
+        });
+});
+
+app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json(Movies.find((movie) => {
+        return movie.title === req.params.title
+    }));
+});
 
 app.get('/', (req, res) => {
-    res.json([1,2,3]);
+    res.contentType('html');
+    res.end('<h1>This is the default</h1>');
 });
 
 const port = process.env.PORT || 5500;
