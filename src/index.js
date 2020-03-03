071297047c4bf739f863842c515ad8c78e7ab1d6
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-03-03 01:50:32 -0800

    Changed db name from test to myFlixDB

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 var allowedOrigins = ['http://localhost:5500', 'http://testsite.com'];
-const CONNECTION_REMOTE_URL = 'mongodb+srv://maxzakh:54ndpaper@movies-my-flix-0j4lo.mongodb.net/test?retryWrites=true&w=majority';
+const CONNECTION_REMOTE_URL = 'mongodb+srv://maxzakh:54ndpaper@movies-my-flix-0j4lo.mongodb.net/myFlixDB?retryWrites=true&w=majority';
 const CONNECTION_LOCAL_URL = 'mongodb://127.0.0.1:27017';
 mongoose.connect(CONNECTION_REMOTE_URL, {
     useUnifiedTopology: true,
