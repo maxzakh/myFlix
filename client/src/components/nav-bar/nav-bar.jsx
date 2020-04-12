eb94fa950d2180db49ed4194e7c51cdd10adb5a0
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-04-11 19:35:59 -0700

    Fixed Navbar UI states

--- client/src/components/nav-bar/nav-bar.jsx
+++ client/src/components/nav-bar/nav-bar.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import { Button, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 import './nav-bar.scss';
 
 export function NavBar(props) {
     const { user, logOut } = props;
+    const location = useLocation();
+    const isLogin = location.pathname === '/login';
+    const isReg = location.pathname === '/register';
 
     return (
         <Navbar className='nav-bar-container shadow-sm' bg='light'>
             {
                 user
                     ?
-                        <div>
-                            <span>
-                                <Link to="/">
-                                    <Button>Movies</Button>
-                                </Link>
-                            </span>
-                            <span>
-                                <Link to="/profile">
-                                    <Button>Profile</Button>
-                                </Link>
-                            </span>
-                            <span><Button onClick={() => logOut()}>Log out</Button></span>
-                        </div>
+                    <div>
+                        <Link to="/">
+                            <Button>Movies</Button>
+                        </Link>
+                        <Link to="/profile">
+                            <Button>Profile</Button>
+                        </Link>
+                        <Button onClick={() => logOut()}>Log out</Button>
+                    </div>
                     :
-                        <div>
-                            <span>
-                                <Link to="/login">
-                                    <Button>Login</Button>
-                                </Link>
-                            </span>
-                            <span>
-                                <Link to="/register">
-                                    <Button>Register</Button>
-                                </Link>
-                            </span>
-                        </div>
+                    <div>
+                        {!isLogin &&
+                            <Link to="/login">
+                                <Button>Login</Button>
+                            </Link>
+                        }
+                        {!isReg &&
+                            <Link to="/register">
+                                <Button>Register</Button>
+                            </Link>
+                        }
+                    </div>
             }
         </Navbar>
     )
