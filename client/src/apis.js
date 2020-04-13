cca2de66f5bc21a6f4a3d6372e3c049fe7d8de1f
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-04-12 20:56:48 -0700

    Updated Heroku address

--- client/src/apis.js
+++ client/src/apis.js
@@ -1,5 +1,5 @@
 // const SERVER_URL = 'http://localhost:5500';
-const SERVER_URL = 'https://movies-my-flix.herokuapp.com';
+const SERVER_URL = 'https://movies-my-flix-server.herokuapp.com';
 
 module.exports = {
     SERVER_URL: SERVER_URL
