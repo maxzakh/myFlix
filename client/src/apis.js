3dde9db2de2388a78454c68ae6a1303c29854cbd
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-04-12 20:46:30 -0700

    Switched calls to heroku server

--- client/src/apis.js
+++ client/src/apis.js
@@ -1,4 +1,5 @@
-const SERVER_URL = 'http://localhost:5500';
+// const SERVER_URL = 'http://localhost:5500';
+const SERVER_URL = 'https://movies-my-flix.herokuapp.com';
 
 module.exports = {
     SERVER_URL: SERVER_URL
